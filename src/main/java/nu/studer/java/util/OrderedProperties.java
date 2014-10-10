9cd39980ad97c9284148c296e841d88178fb3c88
etiennestuder <etienne@studer.nu>
2014-10-10 19:02:26 +0200

    Simplify class DateSuppressingPropertiesBufferedWriter.

--- src/main/java/nu/studer/java/util/OrderedProperties.java
+++ src/main/java/nu/studer/java/util/OrderedProperties.java
@@ -7,23 +7,19 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
-import java.util.Deque;
 import java.util.Enumeration;
 import java.util.InvalidPropertiesFormatException;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
 
 /**
- * This class provides a drop-in replacement for the java.util.Properties class. It fixes the design flaw of using
- * inheritance over composition, while keeping up the same APIs as the original class. As additional functionality, this
- * class keeps its properties in a well-defined order. By default, the order is the one in which the individual
- * properties have been added, either through explicit API calls or through reading them top-to-bottom from a properties
- * file.
+ * This class provides a drop-in replacement for the java.util.Properties class. It fixes the design flaw of using inheritance over composition, while keeping up the same APIs as
+ * the original class. As additional functionality, this class keeps its properties in a well-defined order. By default, the order is the one in which the individual properties
+ * have been added, either through explicit API calls or through reading them top-to-bottom from a properties file.
  *
  * @see Properties
  */
@@ -188,38 +184,37 @@ public final class OrderedProperties {
 
     }
 
+    /**
+     * Custom writer for storing properties that will write all leading lines of comments except
+     * the last comment line. Using the JDK Properties class to store properties, the last comment
+     * line always contains the current date which is what we want to filter out.
+     */
     private static final class DateSuppressingPropertiesBufferedWriter extends BufferedWriter {
 
         private final String LINE_SEPARATOR = System.getProperty("line.separator");
 
-        private boolean inComment;
         private StringBuilder currentComment;
-        private final Deque<String> comments;
+        private String previousComment;
 
         private DateSuppressingPropertiesBufferedWriter(Writer out) {
             super(out);
-
-            inComment = false;
-            currentComment = new StringBuilder();
-            comments = new LinkedList<String>();
         }
 
         @SuppressWarnings("NullableProblems")
         @Override
         public void write(String string) throws IOException {
-            if (inComment) {
+            if (currentComment != null) {
                 currentComment.append(string);
                 if (string.endsWith(LINE_SEPARATOR)) {
-                    comments.addLast(currentComment.toString());
-                    if (comments.size() > 1) {
-                        super.write(comments.removeFirst());
+                    if (previousComment != null) {
+                        super.write(previousComment);
                     }
-                    inComment = false;
+
+                    previousComment = currentComment.toString();
+                    currentComment = null;
                 }
             } else if (string.startsWith("#")) {
-                currentComment.delete(0, currentComment.length());
-                currentComment.append(string);
-                inComment = true;
+                currentComment = new StringBuilder(string);
             } else {
                 super.write(string);
             }
--- src/test/java/nu/studer/java/util/OrderedPropertiesTest.groovy
+++ src/test/java/nu/studer/java/util/OrderedPropertiesTest.groovy
@@ -2,86 +2,85 @@ package nu.studer.java.util
 
 import spock.lang.Specification
 
-
 class OrderedPropertiesTest extends Specification {
 
-    def props = new OrderedProperties()
+  def props = new OrderedProperties()
 
-    def "empty properties"() {
-        setup:
-        assert props.isEmpty()
-    }
+  def "empty properties"() {
+    setup:
+    assert props.isEmpty()
+  }
 
-    def "get property without default value specified"() {
-        setup:
-        props.setProperty("aaa", "111")
-        assert props.getProperty("aaa") == "111"
-        assert props.getProperty("bbb") == null
+  def "get property without default value specified"() {
+    setup:
+    props.setProperty("aaa", "111")
+    assert props.getProperty("aaa") == "111"
+    assert props.getProperty("bbb") == null
 
-        props.setProperty("bbb", null)
-        assert props.getProperty("bbb") == null
-    }
+    props.setProperty("bbb", null)
+    assert props.getProperty("bbb") == null
+  }
 
-    def "get property with default value specified"() {
-        setup:
-        props.setProperty("aaa", "111")
-        assert props.getProperty("aaa", "222") == "111"
-        assert props.getProperty("bbb", "222") == "222"
+  def "get property with default value specified"() {
+    setup:
+    props.setProperty("aaa", "111")
+    assert props.getProperty("aaa", "222") == "111"
+    assert props.getProperty("bbb", "222") == "222"
 
-        props.setProperty("bbb", null)
-        assert props.getProperty("bbb", "222") == "222"
-    }
+    props.setProperty("bbb", null)
+    assert props.getProperty("bbb", "222") == "222"
+  }
 
-    def "properties remain ordered in toString()"() {
-        setup:
-        props.setProperty("bbb", "222")
-        props.setProperty("ccc", "333")
-        props.setProperty("aaa", "111")
+  def "properties remain ordered in toString()"() {
+    setup:
+    props.setProperty("bbb", "222")
+    props.setProperty("ccc", "333")
+    props.setProperty("aaa", "111")
 
-        assert props.toString() == "{bbb=222, ccc=333, aaa=111}"
-    }
+    assert props.toString() == "{bbb=222, ccc=333, aaa=111}"
+  }
 
-    def "properties remain ordered when loading from stream"() {
-        setup:
-        def stream = asStream """\
+  def "properties remain ordered when loading from stream"() {
+    setup:
+    def stream = asStream """\
 b=222
 c=333
 a=111
 """
-        when:
-        props.load(stream)
+    when:
+    props.load(stream)
 
-        then:
-        props.propertyNames().toList() == ["b", "c", "a"]
-        props.stringPropertyNames() == ["b", "c", "a"] as Set
-        props.getProperty("b") == "222"
-        props.getProperty("c") == "333"
-        props.getProperty("a") == "111"
-        props.getProperty("d") == null
-    }
+    then:
+    props.propertyNames().toList() == ["b", "c", "a"]
+    props.stringPropertyNames() == ["b", "c", "a"] as Set
+    props.getProperty("b") == "222"
+    props.getProperty("c") == "333"
+    props.getProperty("a") == "111"
+    props.getProperty("d") == null
+  }
 
-    def "properties remain ordered when loading from reader"() {
-        setup:
-        def reader = asReader """\
+  def "properties remain ordered when loading from reader"() {
+    setup:
+    def reader = asReader """\
 b=222
 c=333
 a=111
 """
-        when:
-        props.load(reader)
+    when:
+    props.load(reader)
 
-        then:
-        props.propertyNames().toList() == ["b", "c", "a"]
-        props.stringPropertyNames() == ["b", "c", "a"] as Set
-        props.getProperty("b") == "222"
-        props.getProperty("c") == "333"
-        props.getProperty("a") == "111"
-        props.getProperty("d") == null
-    }
+    then:
+    props.propertyNames().toList() == ["b", "c", "a"]
+    props.stringPropertyNames() == ["b", "c", "a"] as Set
+    props.getProperty("b") == "222"
+    props.getProperty("c") == "333"
+    props.getProperty("a") == "111"
+    props.getProperty("d") == null
+  }
 
-    def "properties remain ordered when loading from stream as xml"() {
-        setup:
-        def stream = asStream """\
+  def "properties remain ordered when loading from stream as xml"() {
+    setup:
+    def stream = asStream """\
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
 <properties>
@@ -90,67 +89,66 @@ a=111
   <entry key="a">111</entry>
 </properties>
 """
-        when:
-        props.loadFromXML(stream)
+    when:
+    props.loadFromXML(stream)
 
-        then:
-        props.propertyNames().toList() == ["b", "c", "a"]
-        props.stringPropertyNames() == ["b", "c", "a"] as Set
-        props.getProperty("b") == "222"
-        props.getProperty("c") == "333"
-        props.getProperty("a") == "111"
-        props.getProperty("d") == null
-    }
+    then:
+    props.propertyNames().toList() == ["b", "c", "a"]
+    props.stringPropertyNames() == ["b", "c", "a"] as Set
+    props.getProperty("b") == "222"
+    props.getProperty("c") == "333"
+    props.getProperty("a") == "111"
+    props.getProperty("d") == null
+  }
 
+  def "properties remain ordered when writing to stream"() {
+    setup:
+    props.setProperty("b", "222")
+    props.setProperty("c", "333")
+    props.setProperty("a", "111")
+    def stream = new ByteArrayOutputStream()
 
-    def "properties remain ordered when writing to stream"() {
-        setup:
-        props.setProperty("b", "222")
-        props.setProperty("c", "333")
-        props.setProperty("a", "111")
-        def stream = new ByteArrayOutputStream()
+    when:
+    props.store(stream, null)
 
-        when:
-        props.store(stream, null)
-
-        then:
-        stream.toString() endsWith """
+    then:
+    stream.toString() endsWith """
 b=222
 c=333
 a=111
 """
-    }
+  }
 
-    def "properties remain ordered when writing to writer"() {
-        setup:
-        props.setProperty("b", "222")
-        props.setProperty("c", "333")
-        props.setProperty("a", "111")
-        def writer = new StringWriter()
+  def "properties remain ordered when writing to writer"() {
+    setup:
+    props.setProperty("b", "222")
+    props.setProperty("c", "333")
+    props.setProperty("a", "111")
+    def writer = new StringWriter()
 
-        when:
-        props.store(writer, null)
+    when:
+    props.store(writer, null)
 
-        then:
-        writer.toString() endsWith """
+    then:
+    writer.toString() endsWith """
 b=222
 c=333
 a=111
 """
-    }
+  }
 
-    def "properties remain ordered when writing to stream as xml"() {
-        setup:
-        props.setProperty("b", "222")
-        props.setProperty("c", "333")
-        props.setProperty("a", "111")
-        def stream = new ByteArrayOutputStream()
+  def "properties remain ordered when writing to stream as xml"() {
+    setup:
+    props.setProperty("b", "222")
+    props.setProperty("c", "333")
+    props.setProperty("a", "111")
+    def stream = new ByteArrayOutputStream()
 
-        when:
-        props.storeToXML(stream, "foo")
+    when:
+    props.storeToXML(stream, "foo")
 
-        then:
-        stream.toString() == """\
+    then:
+    stream.toString() == """\
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
 <properties>
@@ -160,20 +158,20 @@ a=111
 <entry key="a">111</entry>
 </properties>
 """
-    }
+  }
 
-    def "properties remain ordered when writing to stream as xml with custom encoding"() {
-        setup:
-        props.setProperty("b", "222")
-        props.setProperty("c", "333")
-        props.setProperty("a", "111")
-        def stream = new ByteArrayOutputStream()
+  def "properties remain ordered when writing to stream as xml with custom encoding"() {
+    setup:
+    props.setProperty("b", "222")
+    props.setProperty("c", "333")
+    props.setProperty("a", "111")
+    def stream = new ByteArrayOutputStream()
 
-        when:
-        props.storeToXML(stream, "foo", "ISO-8859-1")
+    when:
+    props.storeToXML(stream, "foo", "ISO-8859-1")
 
-        then:
-        stream.toString() == """\
+    then:
+    stream.toString() == """\
 <?xml version="1.0" encoding="ISO-8859-1" standalone="no"?>
 <!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
 <properties>
@@ -183,72 +181,72 @@ a=111
 <entry key="a">111</entry>
 </properties>
 """
-    }
+  }
 
-    def "date can be suppressed when writing to stream without comment"() {
-        setup:
-        props = OrderedProperties.withoutWritingDateComment()
-        props.setProperty("b", "222")
-        props.setProperty("c", "333")
-        props.setProperty("a", "111")
-        def stream = new ByteArrayOutputStream()
+  def "date can be suppressed when writing to stream without comment"() {
+    setup:
+    props = OrderedProperties.withoutWritingDateComment()
+    props.setProperty("b", "222")
+    props.setProperty("c", "333")
+    props.setProperty("a", "111")
+    def stream = new ByteArrayOutputStream()
 
-        when:
-        props.store(stream, null)
+    when:
+    props.store(stream, null)
 
-        then:
-        stream.toString() == """\
+    then:
+    stream.toString() == """\
 b=222
 c=333
 a=111
 """
-    }
+  }
 
-    def "date can be suppressed for empty set of properties when writing to stream without comment"() {
-        setup:
-        props = OrderedProperties.withoutWritingDateComment()
-        def stream = new ByteArrayOutputStream()
+  def "date can be suppressed for empty set of properties when writing to stream without comment"() {
+    setup:
+    props = OrderedProperties.withoutWritingDateComment()
+    def stream = new ByteArrayOutputStream()
 
-        when:
-        props.store(stream, null)
+    when:
+    props.store(stream, null)
 
-        then:
-        stream.toString() == ""
-    }
+    then:
+    stream.toString() == ""
+  }
 
-    def "date can be suppressed when writing to stream with comment"() {
-        setup:
-        props = OrderedProperties.withoutWritingDateComment()
-        props.setProperty("b", "222")
-        props.setProperty("c", "333")
-        props.setProperty("a", "111")
-        def stream = new ByteArrayOutputStream()
+  def "date can be suppressed when writing to stream with comment"() {
+    setup:
+    props = OrderedProperties.withoutWritingDateComment()
+    props.setProperty("b", "222")
+    props.setProperty("c", "333")
+    props.setProperty("a", "111")
+    def stream = new ByteArrayOutputStream()
 
-        when:
-        props.store(stream, "some comment")
+    when:
+    props.store(stream, "some comment")
 
-        then:
-        stream.toString() == """\
+    then:
+    stream.toString() == """\
 #some comment
 b=222
 c=333
 a=111
 """
-    }
+  }
 
-    def "date can be suppressed for empty set of properties when writing to stream with comment"() {
-        setup:
-        props = OrderedProperties.withoutWritingDateComment()
-        def stream = new ByteArrayOutputStream()
+  def "date can be suppressed for empty set of properties when writing to stream with comment"() {
+    setup:
+    props = OrderedProperties.withoutWritingDateComment()
+    def stream = new ByteArrayOutputStream()
 
-        when:
-        props.store(stream, "some comment")
+    when:
+    props.store(stream, "some comment")
 
-        then:
-        stream.toString() == """\
+    then:
+    stream.toString() == """\
 #some comment
 """
-    }
+  }
 
   def "date can be suppressed when writing to stream with long comment"() {
     setup:
@@ -292,11 +290,11 @@ a=111
   }
 
   private static Reader asReader(String text) {
-        new StringReader(text)
-    }
+    new StringReader(text)
+  }
 
-    private static InputStream asStream(String text) {
-        new ByteArrayInputStream(text.getBytes("ISO-8859-1"))
-    }
+  private static InputStream asStream(String text) {
+    new ByteArrayInputStream(text.getBytes("ISO-8859-1"))
+  }
 
 }
