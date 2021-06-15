20e06db0e8f6f70e84657be4f86809e2b93c2a04
Johannes Bühler <johannes@gradle.com>
2021-06-15 14:01:54 +0200

    Enable gradle remote cache

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,14 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.FailureAction
+import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay.NORMAL
+import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay.PROMPT
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.schedule
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.version
+
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -26,4 +36,18 @@ version = "2021.1"
 
 project {
     description = "Build Cache use case for the Gradle Enterprise trial navigator"
+
+    params {
+        java8Home(Os.linux)
+    }
+
+    val quickFeedbackBuildType = buildType("Quick Feedback") {
+        steps {
+            gradle {
+                tasks = "clean build"
+                buildFile = ""
+                gradleParams = "-s $buildCacheSetup"
+            }
+        }
+    }
 }
---
+++ .teamcity/settings/extensions.kt
@@ -0,0 +1,66 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.CheckoutMode
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.ParametrizedWithType
+import jetbrains.buildServer.configs.kotlin.v2019_2.Project
+import jetbrains.buildServer.configs.kotlin.v2019_2.RelativeId
+import jetbrains.buildServer.configs.kotlin.v2019_2.toId
+
+fun BuildType.agentRequirement(os: Os) {
+    requirements {
+        contains("teamcity.agent.jvm.os.name", os.requirementName)
+    }
+}
+
+fun ParametrizedWithType.java8Home(os: Os) {
+    param("env.JAVA_HOME", "%${os.name}.java8.oracle.64bit%")
+}
+
+const val buildCacheSetup = "--build-cache"
+/**
+ * Creates a new subproject with the given name, automatically deriving the [Project.id] from the name.
+ *
+ * Using this method also implicitly ensures that subprojects are ordered by creation order.
+ */
+fun Project.subProject(projectName: String, init: Project.() -> Unit): Project {
+    val parent = this
+    val subProject = subProject {
+        name = projectName
+        id = RelativeId(name.toId(stripRootProject(parent.id.toString())))
+    }.apply(init)
+
+    this.subProjectsOrder += subProject.id!!
+
+    return subProject
+}
+
+fun Project.buildType(buildTypeName: String, init: BuildType.() -> Unit): BuildType {
+    val buildType = buildType {
+        name = buildTypeName
+        id = RelativeId(name.toId(stripRootProject(this@buildType.id.toString())))
+
+        artifactRules = "build/reports/** => reports"
+        agentRequirement(Os.linux) // default
+
+        params {
+            java8Home(Os.linux)
+
+            param("env.GRADLE_CACHE_REMOTE_URL", "%gradle.cache.remote.url%")
+            param("env.GRADLE_CACHE_REMOTE_USERNAME", "%gradle.cache.remote.username%")
+            param("env.GRADLE_CACHE_REMOTE_PASSWORD", "%gradle.cache.remote.password%")
+            param("env.GRADLE_CACHE_REMOTE_PUSH", "%gradle.cache.remote.push%")
+        }
+
+        vcs {
+            root(DslContext.settingsRoot)
+            checkoutMode = CheckoutMode.ON_AGENT
+        }
+    }.apply(init)
+
+    this.buildTypesOrderIds += buildType.id!!
+    return buildType
+}
+
+fun stripRootProject(id: String): String {
+    return id.replace("${DslContext.projectId.value}_", "")
+}
